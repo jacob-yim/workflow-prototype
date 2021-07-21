30438c0bb028b4bc770b5d35a2537b5420fee4e1
jacob-yim <jayimgs@gmail.com>
2021-07-21 12:20:32 -0700

    initial commit

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/jacob-yim/workflow-prototype
+
+go 1.16
+
+require (
+	k8s.io/apimachinery v0.21.3
+	k8s.io/client-go v0.21.3
+	sigs.k8s.io/controller-runtime v0.9.3
+)
---
+++ hack/update-codegen.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+case "$(uname -s)" in
+    Linux*)     linkutil=readlink;;
+    Darwin*)    linkutil=greadlink;;
+    *)          machine="UNKNOWN:${unameOut}"
+esac
+
+NIRMATA_DIR=$(dirname ${BASH_SOURCE})/..
+NIRMATA_ROOT=$(${linkutil} -f ${NIRMATA_DIR})
+
+CODEGEN_PKG="${GOPATH}/src/k8s.io/code-generator"
+
+NIRMATA_PKG=${NIRMATA_ROOT#"${GOPATH}/src/"}
+
+${CODEGEN_PKG}/generate-groups.sh \
+    "deepcopy,client" \
+    ${NIRMATA_PKG}/pkg/client \
+    ${NIRMATA_PKG}/pkg/apis \
+    "nirmata.com:v1"
